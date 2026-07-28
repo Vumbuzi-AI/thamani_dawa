9bf534fc57d0d0cbf61647005850ffa95d4cdc21
mmunavu <michaelmunavu83@gmail.com>
2026-07-28 19:42:14 +0300

    Fixed dev

--- config/dev.exs
+++ config/dev.exs
@@ -19,7 +19,7 @@ config :thamani_dawa, ThamaniDawa.Repo,
 config :thamani_dawa, ThamaniDawaWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}],
+  http: [port: String.to_integer(System.get_env("PORT") || "5780")],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
